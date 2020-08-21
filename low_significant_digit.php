bf3415a840d97326c995b01a3582d8aa6a7a034a
krivobokov.sv <krivobokov.sv@academia-moscow.ru>
2020-08-21 09:21:05 +0300

    add low significant digit algorithm

--- low_significant_digit.php
+++ low_significant_digit.php
@@ -4,15 +4,43 @@ $length = (int)fgets($fdescr);
 $counts = [];
 $firstNames = [];
 $charIndex = $length - 1;
+
+while ($row = fgets($fdescr)) {
+    $firstName = trim($row);
+    $lastChar = $firstName[$charIndex];
+    $sectionNumber = ord($lastChar);
+    $count = $counts[$sectionNumber] ?? 0;
+    $counts[$sectionNumber] = ++$count;
+    $firstNameSections[$firstName] = $sectionNumber;
+    $firstNames[] = $firstName;
+}
+ksort($counts);
+$sectionMapping = array_combine(array_keys($counts), range(1, count($counts)));
+$counts = array_merge([0], array_values($counts));
+$countsLength = count($counts);
+for ($i = 0; $i < $countsLength - 1; ++$i) {
+    $counts[$i + 1] += $counts[$i];
+}
+$firstNameCount = count($firstNames);
+$res = [];
+for ($i = 0; $i < $firstNameCount; ++$i) {
+    $sectionId = $sectionMapping[$firstNameSections[$firstNames[$i]]];
+    $res[$counts[$sectionId - 1]++] = $firstNames[$i];
+}
+--$charIndex;
+
 while ($charIndex >= 0) {
-    while ($row = fgets($fdescr)) {
-        $firstName = trim($row);
+    $firstNamesLength = count($firstNames);
+    $counts = [];
+    $firstNames = $res;
+    $res = [];
+    for ($i = 0; $i < $firstNamesLength; ++$i) {
+        $firstName = $firstNames[$i];
         $lastChar = $firstName[$charIndex];
         $sectionNumber = ord($lastChar);
         $count = $counts[$sectionNumber] ?? 0;
         $counts[$sectionNumber] = ++$count;
         $firstNameSections[$firstName] = $sectionNumber;
-        $firstNames[] = $firstName;
     }
     ksort($counts);
     $sectionMapping = array_combine(array_keys($counts), range(1, count($counts)));
@@ -27,5 +55,8 @@ while ($charIndex >= 0) {
         $sectionId = $sectionMapping[$firstNameSections[$firstNames[$i]]];
         $res[$counts[$sectionId - 1]++] = $firstNames[$i];
     }
-    ksort($res);
+    --$charIndex;
 }
+ksort($res);
+var_dump($res);
+die('asd');
